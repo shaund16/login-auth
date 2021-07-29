20ac230938d905d180f59e2b19cea75c47a894dc
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-07-28 20:32:40 -0600

    upload files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
-router.get('/', (req, res) => res.send('Welcome to the Main'));
+
+//Welcome Page
+router.get('/', (req, res) => res.render('welcome'));
+
+//Dashboard Page
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+res.render('dashboard', {
+  name: req.user.name
+}));
 
 module.exports = router;
